edd5f1ad4314bbd41b274dbbdf9217e407d129b9
Joeyzh <lma_ma@163.com>
2017-03-24 13:21:38 +0800

    修改项目架构，增加了一个测试module

---
+++ UILibrary/src/main/java/com/joey/ui/base/MainApplication.java
@@ -0,0 +1,40 @@
+package com.joey.ui.base;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.joey.utils.SharedPreferenceUtils;
+
+/**
+ * Created by Administrator on 2016/12/1.
+ */
+public class MainApplication extends Application {
+    private static MainApplication mAppInstance;
+
+    private Context mAppContext;
+    public static MainApplication getInstance(){
+        return mAppInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        MultiDex.install(this);
+        mAppInstance = this;
+        initSDKS();
+        SharedPreferenceUtils.getInstance().init(this,"test");
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    /**
+     * TODO 加载三方SDK
+     */
+    private void initSDKS(){
+
+    }
+}
